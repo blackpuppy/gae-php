88251b72875511fd9d74efc7ae0aafe195ac9889
Zhu Ming <mingzhu.z+github@gmail.com>
2015-08-01 01:03:08 +0800

    Google App Engine tutorial
    - add index page

---
+++ helloworld/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+  <link type="text/css" rel="stylesheet" href="/stylesheets/main.css" />
+</head>
+<body>
+  <h1>Google App Engine PHP Tutorial</h1>
+  <a href="/helloworld">Hello World</a><br>
+  <a href="/hellouser">Hello User</a><br>
+  <a href="/sign">Web Form</a><br>
+</body>
+</html>
